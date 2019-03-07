cf741263e119049739d130ca33f621da167482e7
iz.zp_lemon <zhou.peng@immomo.com>
2019-03-07 14:55:53 +0800

    a

---
+++ src/main/java/com/blessing/aisaka/AisakaApplication.java
@@ -0,0 +1,13 @@
+package com.blessing.aisaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AisakaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blessing/aisaka/AisakaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blessing.aisaka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AisakaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
